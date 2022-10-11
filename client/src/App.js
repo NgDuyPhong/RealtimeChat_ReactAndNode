19903abece4bf0d680320633f63532c6495cac79
DuyPhong <nguyenduyphong997@gitlab.com>
2022-10-12 00:50:05 +0700

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import io from "socket.io-client";
 import _ from "lodash";
 import { useState } from "react";
-// import Chat from "./Chat";
+import Chat from "./Chat";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 
@@ -10,87 +10,102 @@ function App() {
   const socket = io.connect("http://localhost:3001");
   const [userName, setUserName] = useState("");
   const [room, setRoom] = useState("");
+  const [isShowChat, setShowChat] = useState(false);
+  const [idUserChat, setIdUserChat] = useState("");
 
   const joinRoom = () => {
     if (_.isEmpty(userName) || _.isEmpty(room)) return;
     socket.emit("join_room", room);
+    setIdUserChat(`${userName}_${room}_${new Date().getTime()}`);
+    setShowChat(true);
   };
   return (
     <div className="App">
-      <div class="container">
-        <div class="d-flex justify-content-center h-100">
-          <div class="card">
-            <div class="card-header">
+      <div className={`container ${isShowChat && "d-none"}`}>
+        <div className="d-flex justify-content-center h-100">
+          <div className="card">
+            <div className="card-header">
               <h3 className="mt-5">Join A Chat</h3>
-              <div class="d-flex justify-content-end social_icon">
+              <div className="d-flex justify-content-end social_icon">
                 <span>
-                  <i class="fa fa-facebook-square"></i>
+                  <i className="fa fa-facebook-square"></i>
                 </span>
                 <span>
-                  <i class="fa fa-google-plus-square"></i>
+                  <i className="fa fa-google-plus-square"></i>
                 </span>
                 <span>
-                  <i class="fa fa-twitter-square"></i>
+                  <i className="fa fa-twitter-square"></i>
                 </span>
               </div>
             </div>
-            <div class="card-body">
+            <div className="card-body">
               <form>
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fa fa-user"></i>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fa fa-user"></i>
                     </span>
                   </div>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Input your name..."
                     onChange={(event) => {
                       setUserName(event.target.value);
                     }}
                   />
                 </div>
-                <div class="input-group form-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="fa fa-comments-o"></i>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fa fa-comments-o"></i>
                     </span>
                   </div>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Input RoomID..."
                     onChange={(event) => {
                       setRoom(event.target.value);
                     }}
                   />
                 </div>
-                <div class="row align-items-center remember">
+                <div className="row align-items-center remember">
                   <input type="checkbox" />
                   Remember Me
                 </div>
-                <div class="form-group">
-                  <input
-                    type="submit"
-                    value="Join"
-                    class="btn float-right login_btn"
+                <div className="form-group">
+                  <button
+                    type="button"
+                    className="btn float-right login_btn"
                     onClick={joinRoom}
-                  />
+                  >
+                    Join
+                  </button>
                 </div>
               </form>
             </div>
-            <div class="card-footer">
-              <div class="d-flex justify-content-center links">
+            <div className="card-footer">
+              <div className="d-flex justify-content-center links">
                 Don't have an account?<a href="/#">Sign Up</a>
               </div>
-              <div class="d-flex justify-content-center">
-                <a href="/#">Forgot your password?</a>
+              <div className="d-flex justify-content-center">
+                <a href="/#">Forgot your Room?</a>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {/* {isShowChat && ( */}
+      <div className={`${!isShowChat && "d-none"} chat-room`}>
+        <Chat
+          socket={socket}
+          username={userName}
+          room={room}
+          idUser={idUserChat}
+        />
+      </div>
+      {/* )} */}
 
       {/* <h3>Join a chat</h3>
       <input
@@ -108,7 +123,6 @@ function App() {
         }}
       />
       <button onClick={joinRoom}>Join</button> */}
-      {/* <Chat socket={socket} username={userName} room={room} /> */}
     </div>
   );
 }
